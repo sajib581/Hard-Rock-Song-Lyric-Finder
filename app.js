c9766e6299ddc90e5f9f7c343ff3e2fd45559256
sajib581 <sajibdas581@gmail.com>
2020-08-17 23:44:48 +0600

    hard rock done

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ searchBtn.addEventListener('click', function() {
         .then(res => res.json())
         .then(songs => {
             for (let i = 0; i < 10; i++) {
-                //result.innerHTML += `<p class="author lead"><strong>${searchData}</strong> Album by <span>${song.data[i].title}</span> <button class="btn btn-success">Get Lyrics</button></p>`
                 result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-                                        <div class="col-md-9">
+                                        <div class="col-md-6">
                                             <h3 class="lyrics-name">${songs.data[i].title}</h3>
                                             <p class="author lead">Album by <span><strong>${songs.data[i].artist.name}</strong></span></p>
                                         </div>
-                                        <div class="col-md-3 text-md-right text-center">
-                                            <button id="${i}" class="btn btn-success">Get Lyrics</button>
+                                        <div class="col-md-6 text-md-right text-center">
+                                            <button class="btn btn-primary">Play Song</button>
+                                            <button id="${i}" class="btn btn-success ml-1">Get Lyrics</button>
                                         </div>                          
                                     </div>
-                                    <div class="aa">
+                                    <div class="showResult">
 
                                     </div>`
             }
@@ -26,6 +26,7 @@ searchBtn.addEventListener('click', function() {
                 previous = 0;
             }
             for (let i = 0; i < 10; i++) {
+
                 const lyricBtn = document.getElementById(i)
                 lyricBtn.addEventListener("click", function() {
                     const artist = songs.data[i].artist.name;
@@ -34,16 +35,22 @@ searchBtn.addEventListener('click', function() {
                     fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
                         .then(res => res.json())
                         .then(data => {
-                            document.getElementsByClassName("aa")[previous].innerText = "";
-                            document.getElementsByClassName("aa")[i].innerHTML = `<div class="single-lyrics text-center">
+                            if (data.lyrics == undefined) {
+                                document.getElementsByClassName("showResult")[previous].innerHTML = "";
+                                document.getElementsByClassName("showResult")[i].innerHTML = "No data. Please Check above"
+                                previous = i;
+                            } else {
+                                document.getElementsByClassName("showResult")[previous].innerHTML = "";
+                                document.getElementsByClassName("showResult")[i].innerHTML = `<div class="single-lyrics text-center">
                                                                                         <button class="btn go-back">&lsaquo;</button>
                                                                                         <h2 class="text-success mb-4">${title} - ${artist}</h2>
-                                                                                        <pre class="lyric text-white">
-                                                                                        ${data.lyrics}
+                                                                                        <pre class="lyric text-white">                                                                                   
+                                                                                        \n\n${data.lyrics}
                                                                                         </pre>
                                                                                     </div>`
-                            previous = i;
-                            console.log(data.lyrics);
+                                previous = i;
+                                //console.log(data.lyrics);
+                            }
                         })
                 })
             }
